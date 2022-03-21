1c4f547c481ec8d96031a8a3f0e67e92a2ba8859
Theo Fratia <theofratia22@gmail.com>
2022-03-21 15:05:46 +0100

    recuperer le token avec le nom utilisateur et mot de passe entre sur l'app

--- app/src/main/java/com/example/gsb_frais/MainActivity.java
+++ app/src/main/java/com/example/gsb_frais/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.gsb_frais;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.example.gsb_frais.databinding.ActivityMainBinding;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -12,35 +15,47 @@ import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
 
-        GsbServices service = RetrofitClientInstance.getRetrofitInstance().create(GsbServices.class);
-        Visiteur visiteur = new Visiteur("test2@test.com","password");
-        Call<Token> call = service.getToken(visiteur);
-        call.enqueue(new Callback<Token>() {
+        binding.con.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onResponse(Call<Token> call, Response<Token> response) {
-                if(response.code() == 200) {
+            public void onClick(View v) {
+                String username = binding.username.getText().toString();
+                String password = binding.password.getText().toString();
+                GsbServices service = RetrofitClientInstance.getRetrofitInstance().create(GsbServices.class);
+                Call<Token> call = service.getToken(new Visiteur(username,password));
+                call.enqueue(new Callback<Token>() {
+                    @Override
+                    public void onResponse(Call<Token> call, Response<Token> response) {
+                        if(response.code() == 200) {
 
-                    Token token = response.body();
-                    TextView TextviewToken = findViewById(R.id.textviewhelloword);
-                    TextviewToken.setText(token.getToken());
-                    Toast.makeText(MainActivity.this, token.getToken(), Toast.LENGTH_SHORT).show();
-                }
-                else if(response.code() == 401){
-                    Toast.makeText(MainActivity.this, "Erreur de login/mot de passe", Toast.LENGTH_SHORT).show();
-                }
-            }
+                            Token token = response.body();
+                            //TextView TextviewToken = findViewById(R.id.textviewhelloword);
+                            //TextviewToken.setText(token.getToken());
+                            Toast.makeText(MainActivity.this, token.getToken(), Toast.LENGTH_SHORT).show();
+                        }
+                        else if(response.code() == 401){
+                            Toast.makeText(MainActivity.this, "Erreur de login/mot de passe", Toast.LENGTH_SHORT).show();
+                        }
+                    }
 
-            @Override
-            public void onFailure(Call<Token> call, Throwable t) {
-                Toast.makeText(MainActivity.this, "Something went wrong...Please try later!", Toast.LENGTH_SHORT).show();
-                TextView TextviewToken = findViewById(R.id.textviewhelloword);
-                TextviewToken.setText("erreur");
-            }
+                    @Override
+                    public void onFailure(Call<Token> call, Throwable t) {
+                        Toast.makeText(MainActivity.this, "Something went wrong...Please try later!", Toast.LENGTH_SHORT).show();
+                        //TextView TextviewToken = findViewById(R.id.textviewhelloword);
+                        //TextviewToken.setText("erreur");
+                    }
+                });
+
+                }
         });
     }
 }
