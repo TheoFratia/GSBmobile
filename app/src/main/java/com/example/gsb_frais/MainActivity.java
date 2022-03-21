1ce707382ed8491b19b0ea2ddcc2a9d1c756abcf
Theo Fratia <theofratia22@gmail.com>
2022-03-21 17:30:48 +0100

    changer d'activity apres la connexion

--- app/src/main/java/com/example/gsb_frais/MainActivity.java
+++ app/src/main/java/com/example/gsb_frais/MainActivity.java
@@ -2,9 +2,9 @@ package com.example.gsb_frais;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.gsb_frais.databinding.ActivityMainBinding;
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
@@ -38,9 +38,11 @@ public class MainActivity extends AppCompatActivity {
                         if(response.code() == 200) {
 
                             Token token = response.body();
-                            //TextView TextviewToken = findViewById(R.id.textviewhelloword);
-                            //TextviewToken.setText(token.getToken());
-                            Toast.makeText(MainActivity.this, token.getToken(), Toast.LENGTH_SHORT).show();
+                            Intent home = new Intent(MainActivity.this, Home.class);
+                            home.putExtra("token",token.getToken());
+                            home.putExtra("mail",username);
+                            startActivity(home);
+                            //Toast.makeText(MainActivity.this, token.getToken(), Toast.LENGTH_SHORT).show();
                         }
                         else if(response.code() == 401){
                             Toast.makeText(MainActivity.this, "Erreur de login/mot de passe", Toast.LENGTH_SHORT).show();
@@ -50,8 +52,6 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onFailure(Call<Token> call, Throwable t) {
                         Toast.makeText(MainActivity.this, "Something went wrong...Please try later!", Toast.LENGTH_SHORT).show();
-                        //TextView TextviewToken = findViewById(R.id.textviewhelloword);
-                        //TextviewToken.setText("erreur");
                     }
                 });
 
